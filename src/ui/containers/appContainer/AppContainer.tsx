c48b5c3364a2e3eb87cb3c1ebab2abc725adf6ba
Marek Kustosz <markust71@hotmail.com>
2022-01-09 00:50:44 +0100

    add device recognition

--- src/ui/containers/appContainer/AppContainer.tsx
+++ src/ui/containers/appContainer/AppContainer.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Box, useTheme } from '@mui/material';
+import { isMobile } from 'react-device-detect';
 
 import { AppContainerProps } from './AppContainer.types';
 import { useStyles } from './AppContainer.styles';
@@ -8,5 +9,17 @@ export const AppContainer: React.FC<AppContainerProps> = ({ children }) => {
   const theme = useTheme();
   const classes = useStyles(theme);
 
-  return <Box className={classes.container}>{children}</Box>;
+  const MobileContainer: React.FC = isMobile
+    ? ({ children }) => <>{children}</>
+    : ({ children }) => (
+        <Box className={classes.mobileContainer}>
+          <Box className={classes.deviceWrapper}>{children}</Box>
+        </Box>
+      );
+
+  return (
+    <Box className={classes.container}>
+      <MobileContainer>{children}</MobileContainer>
+    </Box>
+  );
 };
